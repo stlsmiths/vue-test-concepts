57fb4b17b2e5b58e10313a8d3e02f6ce125efebe
todd <todd@stlsmiths.com>
2022-07-24 12:25:41 -0500

    initial

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <div>
+    Hello there !!
+    <ul>
+      <li>
+        <a href="https://vitest.dev/api/" target="_blank">Vitest</a>
+      </li>
+      <li><a href="https://pinia.vuejs.org/introduction.html" target="_blank">Pinia</a></li>
+      <li>Vue Test Utils</li>
+    </ul>
+  </div>
+</template>
