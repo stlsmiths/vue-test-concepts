e6bc02f8be7deabd7e15fd8aac26775305d31037
Todd Smith <todd@stlsmiths.com>
2022-07-25 09:33:07 -0500

    cleanup ... [sc-39]

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,29 @@
 <script setup lang="ts">
 import {ref} from 'vue'
-import TheWelcome from '@/components/TheWelcome.vue'
 import MyInput from "@/components/MyInput.vue";
 const mytext = ref<string>()
 </script>
 
 <template>
   <div>
-    <h2>Vue - Test - Pinia</h2>
+    <h1>Vue - Test - Pinia</h1>
     <ul>
       <li>
-        <a href="https://vitest.dev/api/" target="_blank">Vitest</a>
+        <a href="https://vuejs.org/guide/introduction.html" target="_blank">Vue Docs</a>
       </li>
-      <li><a href="https://pinia.vuejs.org/introduction.html" target="_blank">Pinia</a></li>
+      <li>
+        <a href="https://vitest.dev/api/" target="_blank">Vitest API and Matchers</a>
+      </li>
+      <li><a href="https://pinia.vuejs.org/introduction.html" target="_blank">Pinia Docs</a></li>
       <li><a href="https://test-utils.vuejs.org/guide/" target="_blank">Vue Test Utils</a></li>
     </ul>
 
-    <h4>MyInput</h4>
-    <MyInput
-        v-model="mytext"
-    />
-    Value = {{ mytext }}
+    <div style="margin-top: 2rem">
+      <h4>MyInput</h4>
+      <MyInput
+          v-model="mytext"
+      />
+      Value = {{ mytext }}
+    </div>
   </div>
 </template>
