a9592f3f4c1ead210f380d04b26782135300fdc7
Todd Smith <todd@stlsmiths.com>
2022-07-25 09:37:52 -0500

    create myeditor ... [toddsmith1548/sc-123/]

---
+++ src/components/MyEditor.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+</script>
+<template>
+  <div>
+    <h3>MyEditor</h3>
+    <div>
+      Wonderful editing controls await ....
+    </div>
+  </div>
+</template>
