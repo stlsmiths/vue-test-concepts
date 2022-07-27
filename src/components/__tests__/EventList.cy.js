419229fb2d1fbe52eef492deaa26922b172f3972
todd <todd@stlsmiths.com>
2022-07-26 22:41:12 -0500

    got EventList.cy comp test working with testingPinia

--- src/components/__tests__/EventList.cy.js
+++ src/components/__tests__/EventList.cy.js
@@ -1,36 +1,34 @@
+import {createTestingPinia} from "@pinia/testing";
+import { setActivePinia, createPinia } from 'pinia'
+
 import EventList from '../EventList.vue'
 import EventCard from '../EventCard.vue'
 import {events} from '../../../events-db.json'
+import router from '../../router'
 
-import {createTestingPinia} from "@pinia/testing";
-import { setActivePinia, createPinia } from 'pinia'
-import { f } from 'cypress'
-import {useEvents} from '../../stores/events-store'
-import router from "../../router";
-// import {vi} from "vitest/dist";
+let mockEvents = events
 
 const testPinia = createTestingPinia({
   initialState: {
     events: { events }
   },
-  createSpy: f
+  stubActions: true,
+  createSpy: cy.spy
 })
 
 describe('EventList', () => {
-    let mockEvents = events
 
     beforeEach( () => {
         cy.mount(EventList, {
           // components: { EventCard },
           global: {
-            plugins: [ testPinia ]
+            plugins: [ testPinia, router ]
           }
         })
         setActivePinia( createPinia() )
     })
 
     it('should exist and render', () => {
-        // cy.mount(EventCard, { props: { event: mockEvent } })
         cy.getBySel('event-list-title').should('exist')
     })
 
@@ -38,26 +36,21 @@ describe('EventList', () => {
     cy.get('.event-link')
       .should('have.length', 9)
   })
-  it( 'has event card with proper text and href', () => {
+
+  it( 'has first event card with proper text and href', () => {
     const first = cy.get('.event-link')
       .first()
-    first.should('contain', events[0].title)
-    first.should('have.attr','href', '/event/' + events[0].id)
+    first.should('contain', mockEvents[0].title)
+    first.should('have.attr','href')
+        .should('contain','/event/' + mockEvents[0].id)
   })
 
+  it( 'has last event card with proper text and href', () => {
+    const last = cy.get('.event-link').last()
+    const lastIndex = mockEvents.length -1
+    last.should('contain', mockEvents[lastIndex].title)
+    last.should('have.attr','href')
+      .should('contain','/event/' + mockEvents[lastIndex].id)
 
-/*
-    it('should have heading span with date / time', () => {
-        cy.getBySel('event-card')
-            .get('span')
-            .should('have.text', '@12:00 on January 28, 2022')
-            .should('have.text', '@12:00 on January 28, 2022')
-    })
-
-    it('should have correct h4 with title', () => {
-        cy.getBySel('event-card')
-            .get('h4')
-            .should('have.text', 'Cat Adoption Day')
-    })
-*/
+  })
 })
